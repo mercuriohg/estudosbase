38ec292cf0961726ae50814f9dbc5463c4fd3f2d
Arthur Gomes <153852894+mercuriohg@users.noreply.github.com>
2025-09-18 09:40:34 -0300

    Add files via upload

---
+++ Bibliotecas/Teste Java em Aula/Teste Lógico Java/Campeonato/Campeonato.java
@@ -0,0 +1,24 @@
+ public class Campeonato{
+   public Campeonato(){
+       private int idTime;
+       public String nomeTime;
+       private int numJogadores;
+
+
+       public Campeonato(int idTime, String nomeTime, int numJogadores){
+           this.idTime = this.idTime;
+           this.nomeTime = this.nomeTime;
+           this.numJogadores = this.numJogadores;
+       }
+
+       public void setIdTime(int idTime){
+           this.idTime = this.idTime
+       }
+       public int getIdTime(){
+           return this.idTime;
+       }
+       public void setNomeTime(){
+
+       }
+   }
+ }
